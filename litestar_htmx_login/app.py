449da7d1b22d1957dc13a6637d4bd65b12994ff3
Viet Than <thanhoangviet@gmail.com>
2023-09-15 23:32:20 -0400

    yo yo yo

--- litestar_htmx_login/app.py
+++ litestar_htmx_login/app.py
@@ -8,15 +8,38 @@ from litestar.response import Template
 from litestar.contrib.jinja import JinjaTemplateEngine
 from litestar.template.config import TemplateConfig
 from litestar.exceptions import HTTPException
-from litestar.status_codes import HTTP_403_FORBIDDEN, HTTP_201_CREATED, HTTP_200_OK
-from litestar.datastructures import Cookie
+from litestar.status_codes import HTTP_403_FORBIDDEN, HTTP_201_CREATED, HTTP_200_OK, HTTP_302_FOUND
+from litestar.datastructures import Cookie, Header
 from dataclasses import dataclass
+from litestar.connection import ASGIConnection
+from litestar.exceptions import NotAuthorizedException
+from litestar.handlers.base import BaseRouteHandler
 
 from typing import Annotated
 
 from litestar import Litestar, post
 from litestar.enums import RequestEncodingType
 from litestar.params import Body
+from litestar.types import ASGIApp, Scope, Receive, Send
+from litestar.response import Redirect
+from litestar import Request
+from litestar.middleware.base import MiddlewareProtocol
+from litestar.middleware.session.client_side import CookieBackendConfig
+from os import urandom
+from litestar.response.redirect import ASGIRedirectResponse
+
+# we initialize to config with a 16 byte key, i.e. 128 a bit key.
+# in real world usage we should inject the secret from the environment
+session_config = CookieBackendConfig(secret=urandom(16))  # type: ignore[arg-type]
+
+
+
+from litestar.middleware import (
+    AbstractAuthenticationMiddleware,
+    AuthenticationResult,
+)
+from litestar.connection import ASGIConnection
+
 
 from pathlib import Path
 import pprint
@@ -24,6 +47,23 @@ from attrs import define, field
 
 import logging
 LOGGER = logging.getLogger(__name__)
+
+
+class RedirectMiddleware(MiddlewareProtocol):
+    def __init__(self, app: ASGIApp) -> None:
+        super().__init__(app)
+        self.app = app
+
+    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+        if Request(scope).session is None or Request(scope).session == {}:
+            response = ASGIRedirectResponse(path="/login")
+            await response(scope, receive, send)
+        else:
+            await self.app(scope, receive, send)
+
+@get("/", middleware=[RedirectMiddleware])
+async def get_home() -> Template:
+    return HTMXTemplate(template_name="home.html", push_url="/form")
 
 @get("/ping")
 async def ping() -> dict[str, str]:
@@ -43,8 +83,18 @@ class LoginPayload:
 
 @post("/login")
 async def login(data: Annotated[LoginPayload, Body(media_type=RequestEncodingType.MULTI_PART)], request: Request) -> str:
+    c = request.cookies
+    h = request.headers
+    
+    if "auth" in c and c["auth"] == AUTHENTICATION_TOKEN:
+        return Response("ok", cookies=[Cookie(key="auth", value=AUTHENTICATION_TOKEN,httponly=True)], headers={"HX-Redirect" : "/"})
 
-    return "ok"
+    token = await get_token_based_on_login(data.username, data.password)
+
+    if token:
+        return Response("ok", cookies=[Cookie(key="auth", value=token,httponly=True)])
+    
+    return Response("Invalid Username or Password")
 
 
 AUTHENTICATION_TOKEN = '1234'
@@ -87,11 +137,12 @@ async def get_me(request: Request) -> Response:
         raise HTTPException("Not Authorized")
 
 app = Litestar(
-    route_handlers=[ping, get_login, login, get_token, get_me],
+    route_handlers=[get_home, ping, get_login, login, get_token, get_me],
     debug=True,
     request_class=HTMXRequest,
     template_config=TemplateConfig(
         directory=Path("litestar_htmx_login/templates"),
         engine=JinjaTemplateEngine,
     ),
+    middleware=[session_config.middleware]
 )
